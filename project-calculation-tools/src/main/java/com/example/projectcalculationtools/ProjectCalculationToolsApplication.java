647e6a71dede332c59fca0345b48fc5ac6b1b042
nadyaao77 <nadiaao153@gmail.com>
2026-04-29 11:38:22 +0200

    Add files via upload
    intellij

---
+++ project-calculation-tools/src/main/java/com/example/projectcalculationtools/ProjectCalculationToolsApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectcalculationtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCalculationToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectCalculationToolsApplication.class, args);
+    }
+
+}
---
+++ project-calculation-tools/src/test/java/com/example/projectcalculationtools/ProjectCalculationToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectcalculationtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCalculationToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
